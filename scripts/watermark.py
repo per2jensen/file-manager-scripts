118ee04d0de5142770f6c166b45660764b08da3b
Per Jensen <per2jensen@gmail.com>
2023-10-13 20:31:24 +0200

    improved scaling

--- scripts/watermark.py
+++ scripts/watermark.py
@@ -5,6 +5,7 @@ from PIL import Image
 import argparse
 import logging
 import subprocess
+import sys
 
 WATERMARK='© Per Jensen, License: CC BY-NC-SA 4.0'
 WM_WIDTH=700 # matches the copyright notice for pkt 36 for at 2650px wide photo
@@ -18,7 +19,7 @@ watermarks = {}
 
 logging.basicConfig(
     filename="/tmp/watermark.py.log",
-    level=logging.DEBUG,
+    level=logging.INFO,
     format="%(asctime)s:%(levelname)s:%(message)s")
 
 def rename_file(f):
@@ -33,10 +34,14 @@ def main():
     
     for file in args.files:
         im = Image.open(file)
-        pktsize= int(im.width / DEFAULT_WIDTH * DEFAULT_PKTSIZE)
-        wm_width=int(im.width / DEFAULT_WIDTH * WM_WIDTH)
-        wm_height=int(im.width / DEFAULT_WIDTH * WM_HEIGHT)
-        logging.debug("file: '{}', width: '{}', height: '{}', pkt size: '{}'".format(file, im.width, im.height, pktsize))   
+        width = im.width
+        if width < 1100:
+            width = 1100
+
+        pktsize= int(width / DEFAULT_WIDTH * DEFAULT_PKTSIZE)
+        wm_width=int(width / DEFAULT_WIDTH * WM_WIDTH)
+        wm_height=int(width / DEFAULT_WIDTH * WM_HEIGHT)
+        logging.info("file: '{}', width: '{}', height: '{}', watermark pkt size: '{}'".format(file, im.width, im.height, pktsize))   
 
         watermark=None
         try:
@@ -51,34 +56,30 @@ def main():
                 completed_process = subprocess.run([
                 "convert",  "-size",  "{}x{}".format(wm_width, wm_height),  "xc:transparent",  "-font", "NewCenturySchlbk-Italic",
                 "-pointsize", "{}".format(pktsize), "-gravity", "east",
+                "-fill", "black",  "-draw", "text -1+0 \"{}\"".format(WATERMARK),
                 "-fill", "lightgrey", "-draw", "text +0+0 \"{}\"".format(WATERMARK),
-                "-fill", "darkgrey",  "-draw", "text -2+0 \"{}\"".format(WATERMARK),
                 watermark],  check=True, capture_output=True, encoding="utf-8")
                 logging.debug(completed_process)
                 watermarks[pktsize] = watermark
-        except FileNotFoundError as exc:
-            print(f"Process failed because the executable could not be found.\n{exc}")
-        except subprocess.CalledProcessError as exc:
-            logging.error("Generating watermark size: '{}' failed".format(pktsize))
-            logging.error(f"{exc}")
-        except subprocess.TimeoutExpired as exc:
-            logging.error(f"Process timed out.\n{exc}")
 
-
-        try:
             new_filename = rename_file(file)
+            geometry_w = int(width / DEFAULT_WIDTH * 40)
+            geometgy_h = int(width / DEFAULT_WIDTH * 35)
             logging.info("watermarking: file: '{}'".format(file))
             completed_process = subprocess.run([
             "composite", watermark, file,
             "-watermark", "{}x{}".format(WM_BRIGHTNESS, WM_SATURATION),
             "-gravity", "southeast",
-            "-geometry", "+20+20",
+#           "-geometry", "+40+35",
+            "-geometry", "+{}+{}".format(geometry_w, geometgy_h),
             new_filename], check=True, capture_output=True, encoding="utf-8")
             logging.debug(completed_process)
         except FileNotFoundError as exc:
             print(f"Process failed because the executable could not be found.\n{exc}")
+            sys.exit(-1)
         except subprocess.CalledProcessError as exc:
-            logging.error("Stamping watermark failed")
+            logging.error("stderr: {}".format(exc.stderr))
+            logging.error("stdout: {}".format(exc.stdout))
             logging.error(f"{exc}")
         except subprocess.TimeoutExpired as exc:
             logging.error(f"Process timed out.\n{exc}")
