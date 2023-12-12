1b84144b77c6ad5cec74785674845435a3df825b
Per Jensen <per2jensen@gmail.com>
2023-12-12 22:24:09 +0100

    more subdued - account for Orientation

--- scripts/watermark.py
+++ scripts/watermark.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
 
-# License
-# GNU Public License v3.0 or later.
-# See license details here: https://www.gnu.org/licenses/gpl-3.0.html
+'''
+License
+GNU Public License v3.0 or later.
+See license details here: https://www.gnu.org/licenses/gpl-3.0.html
+
+See this for help on exif tag "orientation": 
+https://unix.stackexchange.com/questions/634115/imagemagick-gravity-portrait-photo-frame-of-reference-rotated-90
+'''
+
 
 from PIL import Image
 
@@ -11,13 +17,13 @@ import logging
 import subprocess
 import sys
 
-WATERMARK='© Per Jensen, License: CC BY-NC-SA 4.0'
-WM_WIDTH=700 # matches the copyright notice for pkt 36 for at 2650px wide photo
+WATERMARK='© Per Jensen, license CC BY-NC-SA 4.0'
+WM_WIDTH=700 # matches the copyright notice at pkt 36
 WM_HEIGHT=36
 DEFAULT_WIDTH=2560
 DEFAULT_PKTSIZE=32 # matches a 2560 width photo
-WM_BRIGHTNESS=50
-WM_SATURATION=80
+WM_BRIGHTNESS=30
+WM_SATURATION=50
 
 watermarks = {}
 
@@ -29,6 +35,10 @@ logging.basicConfig(
 def rename_file(f):
     filename = f.rsplit(".", 1)
     return "{}_wm.{}".format(filename[0], filename[1])
+
+def rename_auto_orient_file(f):
+    filename = f.rsplit(".", 1)
+    return "{}_auto_orient.{}".format(filename[0], filename[1])
 
 
 def main():
@@ -59,7 +69,7 @@ def main():
 
                 completed_process = subprocess.run([
                 "convert",  "-size",  "{}x{}".format(wm_width, wm_height),  "xc:transparent",  "-font", "NewCenturySchlbk-Italic",
-                "-pointsize", "{}".format(pktsize), "-gravity", "east",
+                "-pointsize", "{}".format(pktsize), "-gravity", "southeast",
                 "-fill", "black",  "-draw", "text -1+0 \"{}\"".format(WATERMARK),
                 "-fill", "lightgrey", "-draw", "text +0+0 \"{}\"".format(WATERMARK),
                 watermark],  check=True, capture_output=True, encoding="utf-8")
@@ -70,11 +80,25 @@ def main():
             geometry_w = int(width / DEFAULT_WIDTH * 40)
             geometgy_h = int(width / DEFAULT_WIDTH * 35)
             logging.info("watermarking: file: '{}'".format(file))
+
+            # check if Exif Orientation is "TopLeft", if not use IM -auto-orient feature on a copied image
+            cp =  subprocess.run([
+            "identify", "-format", r"'%[orientation]'", file],
+            check=True, capture_output=True, encoding="utf-8")
+            logging.debug(cp)
+            if (cp.stdout.find("TopLeft") == -1): #Exif Orientation is not TopLeft, let's fix that
+                file_auto_orient = rename_auto_orient_file(file)
+                cp =  subprocess.run([
+                "convert", "-auto-orient", file, file_auto_orient], # don't change original file
+                check=True, capture_output=True, encoding="utf-8")
+                logging.debug(cp)
+                file = file_auto_orient  
+
+            # composite watermark and image
             completed_process = subprocess.run([
             "composite", watermark, file,
             "-watermark", "{}x{}".format(WM_BRIGHTNESS, WM_SATURATION),
             "-gravity", "southeast",
-#           "-geometry", "+40+35",
             "-geometry", "+{}+{}".format(geometry_w, geometgy_h),
             new_filename], check=True, capture_output=True, encoding="utf-8")
             logging.debug(completed_process)
